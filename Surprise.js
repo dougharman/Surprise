2491c4474f6f97ef985b451595e432e318b4f436
giteup <dougharman@Douglass-MacBook-Pro.local>
2013-12-31 14:52:40 -0600

    2013_12_31 - Update, last night's & this am's work

--- Surprise.js
+++ Surprise.js
@@ -1,23 +1,46 @@
+$(document).ready(function () {
+   
+    $(".text_1").fadeIn(3000);
+    $(".surprise").delay(3000).fadeIn(3000);
+    $(".jib").delay(4000).fadeIn(3000);
 
-$(document).ready(function() {
+   $(".name").setInterval(function () {
+        $(".name1, .name2, .name3, .name4, .name5, .name6").delay(200 * i).fadeIn(200);
+        });
+/*
+setInterval(function(){alert("Hello")},3000);
 
-	$(".text_1").fadeIn("3000", function () {
-		var isVisible_1 = $(".text_1").is(":visible");
-		if ( isVisible_1 === true ) {
-			$("h1").delay(1000).fadeIn("fast");
-		}
-		});
-
-// I tried putting this inside (a) a function, e.g., $(this).load...  and (b) an if statement, e.g, if ("h2:visible").load...  No luck!!!
-// I want to delay it until the title / "Surprise" is shown	
-    $.each($(".navigation li"), function (i, item) {
-        setTimeout(function () {
-            $(item).delay(1000).fadeIn(2000);
-        }, 2000 * i);
-	});  
-
-	$(".text_2").delay(13000).fadeIn("slow");	
-	$(".text_3").delay(13000).fadeIn("slow");
-			
+  $("#element3,#element2,#element1").each(function(i) {
+        $(this).delay(600*i).animate({opacity: "1"}, "slow");
+    });
 });
-  
+
+setTimeout(function(){
+    ...
+ },100 * idx);
+
+*/
+    $(".name6:visible").trigger(function() {
+        $(".text_2, .text_3").fadeIn("slow");
+    });
+   
+
+
+
+/*
+    $("#inputBox").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            
+        }
+
+    });
+
+// Save the player's guess if player clicks on "Submit"
+    $("#submitButton").click(function () {
+        event.preventDefault();
+      
+    });
+*/
+
+});
