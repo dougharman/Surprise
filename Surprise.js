27a28bfed836945a6d94e2951859d1ce88d7286e
giteup <dougharman@Douglass-MacBook-Pro.local>
2014-01-02 21:12:50 -0600

    2014_01_02 - Today's work

--- Surprise.js
+++ Surprise.js
@@ -41,18 +41,84 @@ $(document).ready(function () {
 // Click a navigation picture to see more of the collection
     $(".nav1").click(function () {
             event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1_Scary.html";
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-1_Scary.html";
+    });
+
+    $(".nav2").click(function () {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-2_Comic.html";
+    });
+
+    $(".nav3").click(function () {
+                event.preventDefault();
+                window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-1_Animals.html";
+        });
+
+    $(".nav4").click(function () {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-2_Holiday.html";
+    });
+
+    $(".nav5").click(function () {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-1_Political.html";
+    });
+
+    $(".nav6").click(function () {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-2_Emphemera.html";
     });
 
 // Press "Enter" on a navigation picture to see more of the collection    
     $(".nav1").keydown(function (event) {
         if (event.which == 13) {
             event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1_Scary.html";
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-1_Scary.html";
         }
 
     });
 
+    $(".nav2").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-2_Comic.html";
+        }
+
+    });
+
+    $(".nav2").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-2_Animals.html";
+        }
+
+    });
+
+    $(".nav2").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-2_Holiday.html";
+        }
+
+    });
+
+    $(".nav2").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-1_Political.html";
+        }
+
+    });
+
+    $(".nav2").keydown(function (event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-2_Emphemera.html";
+        }
+
+    });
+
+    
 /*    $( "select" )
       .change(function () {
         var str = "";
