8aa6c2fca39aee2761b009e46d4ecbe931d7e283
giteup <dougharman@Douglass-MacBook-Pro.local>
2014-01-04 23:13:56 -0600

    2014_01_04 - Update - today's work, 11:13pm

--- Surprise.js
+++ Surprise.js
@@ -1,22 +1,47 @@
+var loadedNum = 1;
+
+$(function(){
+         var loaded = parseInt(localStorage.getItem('loaded'), 10);
+         var loadedNum = loaded+1;
+         localStorage.setItem('loaded', loadedNum);
+
+         console.log(loadedNum);
+    });
+
 $(document).ready(function () {
    
-    $(".text_1").fadeIn(3000);
-    $(".surprise").fadeIn(3000);
-    $(".jib").delay(4000).fadeIn(3000);
+    if (loadedNum > 1) {
+        $(".title, .nav, .name, .text").fadeIn("fast");
 
-    setInterval(function () {
-        $(".nav1, .nav2, .nav3, .nav4, .nav5, .nav6").each(function(i) {
-            $(this).delay(600*i).fadeIn("slow");
-            });
-        
-        $(".name1, .name2, .name3, .name4, .name5, .name6").fadeIn(600);
+    } else {
+        $(".text_1").fadeIn(1000);
+        $(".surprise").fadeIn(3000);
+        $(".jib").delay(1000).fadeIn(3000);
 
-        }, 10000);
+        setInterval(function () {
+            $(".nav1, .nav2, .nav3, .nav4, .nav5, .nav6").each(function(i) {
+                $(this).delay(600*i).fadeIn("slow");
+                });
+            
+            $(".name1, .name2, .name3, .name4, .name5, .name6").fadeIn(600);
 
-    setInterval(function () {
-        $(".text_2, .text_3").fadeIn("slow");
+            }, 4000);
 
-        }, 11000);
+
+        setInterval(function () {
+            $(".nav7, .nav8, .nav9, .nav10, .nav11, .nav12").each(function(i) {
+                $(this).delay(600*i).fadeIn("slow");
+                });
+            
+            $(".name7, .name8, .name9, .name10, .name11, .name12").fadeIn(600);
+
+            }, 4000);
+
+        setInterval(function () {
+            $(".text_2, .text_3").fadeIn("slow");
+
+            }, 7000);
+    } 
 
 // Tooltip only Text
     $('.masterToolTip').hover(function(){
@@ -37,98 +62,6 @@ $(document).ready(function () {
             $('.tooltip')
             .css({ top: mousey, left: mousex })
     });
-
-// Click a navigation picture to see more of the collection
-    $(".nav1").click(function () {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-1_Scary.html";
-    });
-
-    $(".nav2").click(function () {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-2_Comic.html";
-    });
-
-    $(".nav3").click(function () {
-                event.preventDefault();
-                window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-1_Animals.html";
-        });
-
-    $(".nav4").click(function () {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-2_Holiday.html";
-    });
-
-    $(".nav5").click(function () {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-1_Political.html";
-    });
-
-    $(".nav6").click(function () {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-2_Emphemera.html";
-    });
-
-// Press "Enter" on a navigation picture to see more of the collection    
-    $(".nav1").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-1_Scary.html";
-        }
-
-    });
-
-    $(".nav2").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav1-2_Comic.html";
-        }
-
-    });
-
-    $(".nav2").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-2_Animals.html";
-        }
-
-    });
-
-    $(".nav2").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav2-2_Holiday.html";
-        }
-
-    });
-
-    $(".nav2").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-1_Political.html";
-        }
-
-    });
-
-    $(".nav2").keydown(function (event) {
-        if (event.which == 13) {
-            event.preventDefault();
-            window.location="file:///Users/dougharman/Dropbox/Public/Surprise/Nav3-2_Emphemera.html";
-        }
-
-    });
-
-    
-/*    $( "select" )
-      .change(function () {
-        var str = "";
-        $( "select option:selected" ).each(function() {
-          str += $( this ).text() + " ";
-        });
-        $( "div" ).text( str );
-      })
-      .change();    */
-
 
 });
 
